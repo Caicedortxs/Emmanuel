8f4a8bc2e9d9097cb680145739388e3629af792e
Caicedortxs <gamerecaicedo@gmail.com>
2023-05-05 11:32:23 -0500

    cambios

---
+++ js/media-query.js
@@ -0,0 +1,8 @@
+let  menuBar = document.getElementById("menuBar")
+
+menuBar.addEventListener("click", function(){
+    alert("buenos días")
+} )
+
+alert("Bienvenidos a Recildeas, recicla, reutiliza y crea")
+
