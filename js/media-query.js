86875522c14e785943bf48571257c8c98b120c3d
Caicedortxs <gamerecaicedo@gmail.com>
2023-05-12 11:46:26 -0500

    cambios

--- js/media-query.js
+++ js/media-query.js
@@ -1,7 +1,20 @@
-let  menuBar = document.getElementById("menuBar")
+let mainNav = document.getElementById("mainNav");
+let menuBar = document.getElementById("menuBar");
+let mainMenu = document.getElementById("mainMenu")
 
 menuBar.addEventListener("click", function(){
-    alert("buenos días")
+    mainNav.classList.add("main-nav-2");
+    if(mainMenu.classList.contains("main-menu")){
+        mainMenu.classList.remove("main-menu")
+        mainMenu.classList.add("main-menu-block")
+        
+    }
+    else{
+        mainMenu.classList.add("main-menu");
+        mainMenu.classList.remove("main-menu-block");
+    }
+    mainNav.classList.remove("main-nav")
+    mainMenu.classList.add("main-nav-2")
 } )
 
 alert("Para muertos de miedo")
