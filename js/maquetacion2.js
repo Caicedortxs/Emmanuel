c40c91ebfd9900361370a2467de04a9d4917b7db
Caicedortxs <gamerecaicedo@gmail.com>
2023-08-11 11:44:59 -0500

    cambios

--- js/maquetacion2.js
+++ js/maquetacion2.js
@@ -1,14 +1,19 @@
-let menuBar = document.getElementById("menuBar")
 let mainNav = document.getElementById("mainNav")
-let mainMenu =document.getElementById("mainMenu")
+let menuBar = document.getElementById("menuBar")
+let mainMenu = document.getElementById("mainMenu");
+
 
 menuBar.addEventListener("click", function() {
-if ( mainMenu.classList.contains("main-menu")) {
-     mainMenu.classList.add("main-menu-block");
-     mainMenu.classList.remove("main-menu");
-}
-else {
-     mainMenu.classList.add("main-menu");
-     mainMenu.classList.remove("main-menu-block");
-} 
+    if (mainMenu.classList.contains("main-menu-pc")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu-pc");
+        mainMenu.classList.remove("main-menu-block-hidden");
+    }
+    else {
+        mainMenu.classList.add("main-menu-pc");
+        mainMenu.classList.add("main-menu-block-hidden");
+        mainMenu.classList.remove("main-menu-block");
+    }
+    mainMenu.classList.remove("main-nav");
+    mainMenu.classList.add("main-nav-2");
 });
